fbf4223964ebfb6130a924a59913e76a52b56510
YaroslavBojko <144372173+YaroslavBojko@users.noreply.github.com>
2023-12-20 19:39:08 +0300

    portfolio section added

--- test 1.cpp
+++ test 1.cpp
@@ -4,4 +4,5 @@ int main {
     int a = 2 + 2;
     std::cout << "Hello Git!!!" << a <<  std::endl;
     std::cout << "Services::Портфолио" << std::endl;
+    std::cout << "Portfolio" << std::endl;
 }
