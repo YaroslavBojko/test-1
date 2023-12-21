710a3dbd387da2689d397a39a32f055163ee96e0
YaroslavBojko <144372173+YaroslavBojko@users.noreply.github.com>
2023-12-21 18:21:35 +0300

    1 portfolio menu item (#2)
    * portfolio item added to menu
    
    * portfolio section added
    
    * wrong link corrected

--- test 1.cpp
+++ test 1.cpp
@@ -3,4 +3,6 @@
 int main {
     int a = 2 + 2;
     std::cout << "Hello Git!!!" << a <<  std::endl;
+    std::cout << "Portfolio::Портфолио" << std::endl;
+    std::cout << "Portfolio" << std::endl;
 }
