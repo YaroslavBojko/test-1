a821d510d4dc03cdbda5fd92e9d91d4a453caa46
YaroslavBojko <144372173+YaroslavBojko@users.noreply.github.com>
2024-01-13 15:14:24 +0300

    4 (#3)

--- test 1.cpp
+++ test 1.cpp
@@ -4,5 +4,4 @@ int main {
     int a = 2 + 2;
     std::cout << "Hello Git!!!" << a <<  std::endl;
     std::cout << "Portfolio::Портфолио" << std::endl;
-    std::cout << "Portfolio" << std::endl;
 }
